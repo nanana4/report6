fb313a3828159d15263aa8b12c4a2398287b1aee
nanana4 <e245724@ie.u-ryukyu.ac.jp>
2025-01-22 02:57:44 +0900

    5th commit

--- app/src/main/java/jp/ac/uryukyu/ie/e245724/Main.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e245724/Main.java
@@ -2,31 +2,22 @@ package jp.ac.uryukyu.ie.e245724;
 
 import java.util.Scanner;
 
-/**
- * じゃんけんゲームを実行するクラス。
- */
 public class Main {
     public static void main(String[] args) {
-        Janken janken = new Janken();
+        Janken janken = new Janken(); // Jankenクラスのインスタンスを生成
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("じゃんけんゲームへようこそ！");
         System.out.println("あなたの手を入力してください: 0 (グー), 1 (パー), 2 (チョキ)。");
-
         System.out.print("あなたの選択: ");
         int playerChoice = scanner.nextInt();
 
-        if (playerChoice < 0 || playerChoice > 2) {
-            System.out.println("無効な選択です。ゲームを再起動してください。");
-            return;
-        }
-
         int computerChoice = janken.getComputerChoice();
-        System.out.println("コンピュータの選択: " + janken.choiceToString(computerChoice));
-
         String result = janken.determineWinner(playerChoice, computerChoice);
-        System.out.println("結果: あなたは" + result + "でした！");
 
-        scanner.close();
+        System.out.println("コンピュータの選択: " + janken.choiceToString(computerChoice));
+        System.out.println("結果: あなたは" + result + "しました！");
+
+        scanner.close(); // Scannerを明示的に閉じる
     }
 }
